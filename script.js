a50e8e4fe7673d21a5cf644657c8e23bb159d0d9
Sanjay <94177314+Sanjay1907@users.noreply.github.com>
2022-04-27 21:30:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function sendEmail(){
         To : document.getElementById("email").value,
         From : 'imsanju1907@gmail.com',
         Subject : "New contact enquiry form",
-        Body : "Name : " + document.getElementById("name").value
-        +"<br> Email : " + document.getElementById("email").value
-        +"<br> Message : " + document.getElementById("message").value,
+        Body : "Hi" + document.getElementById("name").value
+        +"<br> Thank you for connecting with me. I like to hear more from you."
+        +"<br> Best regards"
+        +"<br> Sanjay.A",
     }).then(
       message => alert("Message sent succesfully")
     );
